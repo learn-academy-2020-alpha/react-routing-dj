d75b1c964bd4f59975cd82292d8ebb140b637a45
jonas-fernandes <57605686+jonas-fernandes@users.noreply.github.com>
2020-03-23 12:05:45 -0700

    Second Commit

--- fav_movie/src/App.js
+++ fav_movie/src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch
+} from "react-router-dom"
+import Home from "./components/Home1"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+    return (
+      {/* 3. The set of links and paths are all wrapped in the "BrowserRouter as Router" component */}
+        <Router>
+            <div>
+                <h1>Here is App.js Handling Our Routing</h1>
+                {/* 4. Using a semantic tag to denote the navigation links */}
+                <nav>
+                    <ul>
+                    <li>
+                        {/* 5. Creating the anchor tags using the Link component from React Router */}
+                        <Link to="/">Home</Link>
+                    </li>
+                    </ul>
+                </nav>
+
+                {/* 6. Calling the components we imported from "pages" to render in response to the Link */}
+                <Route path="/" exact component={ Home } />
+            </div>
+        </Router>
+    )
 }
+export default App
 
 export default App;
